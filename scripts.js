2c32aaa5c385cd417880734aab649c516949eb1f
Yash Bughani <20314553+yashbughani@users.noreply.github.com>
2021-12-06 10:40:12 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function () {
+    let nav = document.querySelector('nav');
+    let windowPosition = window.scrollY > 0;
+    nav.classList.toggle('scrolling-active', windowPosition);
+})
