7e91f565a96f9bbe914b9b57bd930ca5c5cce71c
Yash Bughani <20314553+yashbughani@users.noreply.github.com>
2021-12-06 14:29:19 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
+const cursor = document.querySelector('.cursor');
+
 window.addEventListener('scroll', function () {
     let nav = document.querySelector('nav');
     let windowPosition = window.scrollY > 0;
     nav.classList.toggle('scrolling-active', windowPosition);
+    cursor.classList.toggle('cursor-off', windowPosition);
+    document.querySelector('body').classList.toggle('pointer', windowPosition);
 })
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
+
+
+
